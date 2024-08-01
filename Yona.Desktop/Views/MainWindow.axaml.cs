6664da818034b9e8d795a465d6173ce83484bfbe
MirrorTuneZM <zutomayofan50@gmail.com>
2024-07-31 19:08:48 -0500

    Add project files.

---
+++ Yona.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Yona.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
