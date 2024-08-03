f995a8b1472d9539a8ab4f5c5511281d88c8f750
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-02 19:27:19 -0500

    Implement settings vm.

---
+++ Yona.Library/ViewModels/Dashboard/Settings/SettingsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Yona.Library.Settings;
+using Yona.Library.Settings.Models;
+
+namespace Yona.Library.ViewModels.Dashboard.Settings;
+
+public partial class SettingsViewModel : ViewModelBase
+{
+    private ThemeColorOption _selectedColor;
+
+    public SettingsViewModel(SettingsService settings)
+    {
+        this.Settings = settings;
+        this._selectedColor = this.ColorOptions.First(x => x.Color == this.Settings.Current.ThemeColor);
+    }
+
+    public SettingsService Settings { get; }
+
+    public ThemeColorOption[] ColorOptions { get; } = ThemeColorOption.AvailableOptions;
+
+    public ThemeColorOption SelectedColor
+    {
+        get => this._selectedColor;
+        set
+        {
+            this.SetProperty(ref this._selectedColor, value);
+            this.Settings.Current.ThemeColor = this._selectedColor.Color;
+        }
+    }
+
+    public PageType[] Pages { get; } = Enum.GetValues<PageType>();
+
+    public PageType SelectedPage
+    {
+        get => this.Settings.Current.StartPage;
+        set => this.SetProperty(this.Settings.Current.StartPage, value, this.Settings.Current, (s, n) => s.StartPage = n);
+    }
+
+    [RelayCommand]
+    private void Reset() => this.Settings.Reset();
+}
---
+++ Yona.Library/ViewModels/Dashboard/Settings/ThemeColorOption.cs
@@ -0,0 +1,14 @@
+﻿using Yona.Library.Settings.Models;
+
+namespace Yona.Library.ViewModels.Dashboard.Settings;
+
+public record ThemeColorOption(string Name, ThemeColor Color)
+{
+    public static readonly ThemeColorOption[] AvailableOptions =
+    [
+        new("Blue", ThemeColor.Blue),
+        new("Red", ThemeColor.Red),
+        new("Green", ThemeColor.Green),
+        new("Orange", ThemeColor.Orange),
+    ];
+}
