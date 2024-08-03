354112359f12587fee813f5c3bd8edeb0ca91d58
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-02 19:26:31 -0500

    Implement settings view.

---
+++ Yona.Desktop/Views/Dashboard/Settings/SettingsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Yona.Desktop.Views.Dashboard.Settings;
+
+public partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
