6353278da359b83ed92f2ae9c6b3a9e967728d7f
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-02 19:32:16 -0500

    Setup theme switch. Update main window.

--- Yona.Desktop/App.axaml.cs
+++ Yona.Desktop/App.axaml.cs
@@ -3,29 +3,41 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
+using SukiUI;
+using SukiUI.Enums;
 using System;
+using System.ComponentModel;
 using Yona.Desktop.Extensions;
 using Yona.Desktop.Views;
+using Yona.Library.Settings;
+using Yona.Library.Settings.Models;
 using Yona.Library.ViewModels;
+using Yona.Library.ViewModels.Dashboard;
 
 namespace Yona.Desktop;
 
 public partial class App : Application
 {
     private readonly IServiceProvider serviceProvider;
+    private readonly SettingsService settings;
 
     public App()
     {
         var services = new ServiceCollection();
 
         services.AddViewModels();
+        services.AddServices();
+        services.AddLoggers();
 
         this.serviceProvider = services.BuildServiceProvider();
+        this.settings = this.serviceProvider.GetRequiredService<SettingsService>();
     }
 
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+        this.SetupTheming();
     }
 
     public override void OnFrameworkInitializationCompleted()
@@ -37,6 +49,9 @@ public partial class App : Application
             BindingPlugins.DataValidators.RemoveAt(0);
 
             var mainWindow = this.serviceProvider.GetRequiredService<MainWindowViewModel>();
+            var dashboard = this.serviceProvider.GetRequiredService<DashboardViewModel>();
+            mainWindow.RootViewModel = dashboard;
+
             desktop.MainWindow = new MainWindow
             {
                 DataContext = mainWindow,
@@ -45,4 +60,27 @@ public partial class App : Application
 
         base.OnFrameworkInitializationCompleted();
     }
+
+    private void SetupTheming()
+    {
+        var sukiTheme = SukiTheme.GetInstance();
+        sukiTheme.ChangeColorTheme(this.GetSukiColor(settings.Current.ThemeColor));
+
+        settings.Current.PropertyChanged += Settings_PropertyChanged;
+    }
+
+    private void Settings_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        var sukiTheme = SukiTheme.GetInstance();
+        sukiTheme.ChangeColorTheme(this.GetSukiColor(settings.Current.ThemeColor));
+    }
+
+    private SukiColor GetSukiColor(ThemeColor color) => color switch
+    {
+        ThemeColor.Blue => SukiColor.Blue,
+        ThemeColor.Orange => SukiColor.Orange,
+        ThemeColor.Green => SukiColor.Green,
+        ThemeColor.Red => SukiColor.Red,
+            _ => SukiColor.Blue,
+    };
 }
