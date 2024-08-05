dcf1c8733c358d715852e9b1824ff2bf463759d7
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-04 23:55:53 -0500

    Add project/template icon converter.

---
+++ Yona.Desktop/Common/IconUtils.cs
@@ -0,0 +1,42 @@
+﻿using Yona.Library.Common;
+
+namespace Yona.Desktop.Common;
+
+internal static class IconUtils
+{
+    public static string? GetDefaultIconFromName(string name)
+    {
+        if (GetGameName(name) is string gameName)
+        {
+            var gameIconName = gameName.ToLower().Replace(" ", "-");
+            return gameIconName;
+        }
+
+        return null;
+    }
+
+    private static string? GetGameName(string name)
+    {
+        if (name.Contains(GameName.P5R, System.StringComparison.OrdinalIgnoreCase))
+        {
+            return GameName.P5R;
+        }
+
+        if (name.Contains(GameName.P4G, System.StringComparison.OrdinalIgnoreCase))
+        {
+            return GameName.P4G;
+        }
+
+        if (name.Contains(GameName.P3R, System.StringComparison.OrdinalIgnoreCase))
+        {
+            return GameName.P3R;
+        }
+
+        if (name.Contains(GameName.P3P, System.StringComparison.OrdinalIgnoreCase))
+        {
+            return GameName.P3P;
+        }
+
+        return null;
+    }
+}
---
+++ Yona.Desktop/Converters/ProjectIconConverter.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
+using System.Globalization;
+using Yona.Desktop.Common;
+using Yona.Library.Projects.Models;
+
+namespace Yona.Desktop.Converters;
+
+public class ProjectIconConverter : IValueConverter
+{
+    private static readonly Uri appIcon = new("avares://Yona.Desktop/Assets/Icons/logo-icon.webp");
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Project project)
+        {
+            return null;
+        }
+        else if (value is ProjectTemplate template)
+        {
+            if (IconUtils.GetDefaultIconFromName(template.Name) is string internalName)
+            {
+                var iconUri = new Uri($"avares://Yona.Desktop/Assets/Icons/{internalName}.webp");
+
+                try
+                {
+                    return new Bitmap(AssetLoader.Open(iconUri));
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            return new Bitmap(AssetLoader.Open(appIcon));
+        }
+
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return new NotSupportedException();
+    }
+}
