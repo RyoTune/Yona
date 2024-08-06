7abe3d054871e813cc8588c58132d22a0c4d5ec2
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-06 16:39:30 -0500

    Add ID to project template.

---
+++ Yona.Core/Common/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Yona.Core.Common.Interfaces;
+
+internal interface IRepository<TEntity, TId>
+    where TEntity : class, new()
+{
+    void Add(TEntity entity);
+
+    void Delete(TEntity entity);
+
+    void Update(TEntity entity);
+
+    TEntity Get(TId id);
+
+    IReadOnlyList<TEntity> Items { get; }
+}
--- Yona.Core/Projects/Models/ProjectTemplate.cs
+++ Yona.Core/Projects/Models/ProjectTemplate.cs
@@ -5,6 +5,9 @@ namespace Yona.Core.Projects.Models;
 public partial class ProjectTemplate : ObservableObject
 {
     [ObservableProperty]
+    public string id = Guid.NewGuid().ToString();
+
+    [ObservableProperty]
     private string name = string.Empty;
 
     [ObservableProperty]
