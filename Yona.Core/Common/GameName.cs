30e37413020cbe96ac117f0d39985db34da76818
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-05 23:09:59 -0500

    Add game name constants.

---
+++ Yona.Core/Common/GameName.cs
@@ -0,0 +1,9 @@
+﻿namespace Yona.Core.Common;
+
+public static class GameName
+{
+    public const string P5R = "Persona 5 Royal";
+    public const string P4G = "Persona 4 Golden";
+    public const string P3R = "Persona 3 Reload";
+    public const string P3P = "Persona 3 Portable";
+}
