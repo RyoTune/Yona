afe92b99b8e43d291a4881058b57534052044afe
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-05 22:43:42 -0500

    Add settings service.

---
+++ Yona.Core/Settings/SettingsService.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel;
+using Yona.Core.Common;
+using Yona.Core.Common.Serializers;
+using Yona.Core.Settings.Models;
+
+namespace Yona.Core.Settings;
+
+public class SettingsService : ObservableObject
+{
+    private readonly ILogger log;
+    private readonly SavableFile<AppSettings> settings;
+
+    public SettingsService(ILogger log)
+    {
+        this.log = log;
+
+        var appDir = AppDomain.CurrentDomain.BaseDirectory;
+        var settingsFile = Path.Join(appDir, "settings.json");
+        settings = new SavableFile<AppSettings>(settingsFile, JsonFileSerializer.Instance);
+
+        Current.PropertyChanged += OnSettingsChanged;
+    }
+
+    public AppSettings Current => settings.Value;
+
+    public void Reset()
+    {
+        Current.PropertyChanged -= OnSettingsChanged;
+
+        settings.Value = new();
+        settings.Save();
+        OnPropertyChanged(nameof(Current));
+
+        Current.PropertyChanged += OnSettingsChanged;
+    }
+
+    private void OnSettingsChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        try
+        {
+            settings.Save();
+            this.log.LogDebug("Settings saved.");
+        }
+        catch (Exception ex)
+        {
+            this.log.LogError(ex, "Failed to auto-save settings.");
+        }
+    }
+}
