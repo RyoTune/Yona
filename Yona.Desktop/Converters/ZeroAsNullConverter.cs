328afaa1327f8471f6e2af2092f7b038b9049a6b
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-06 21:00:45 -0500

    Add converter to treat zero as null.

---
+++ Yona.Desktop/Converters/ZeroAsNullConverter.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Yona.Desktop.Converters;
+
+internal class ZeroAsNullConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int number)
+        {
+            return (number == 0) ? null : number;
+        }
+
+        return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
