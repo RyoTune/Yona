f66d51c69c8cbd5f4466e66d279a4dec8a547cbc
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-08 14:18:13 -0500

    Add VM file select options.

---
+++ Yona.Core/Common/Dialog/FileSelectOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Yona.Core.Common.Dialog;
+
+public class FileSelectOptions
+{
+    public string? Title { get; set; }
+
+    public bool AllowMultiple { get; set; }
+
+    public string? SuggestedStartLocation { get; set; }
+
+    public string? SuggestedFileName { get; set; }
+
+    public string? Filter { get; set; }
+}
