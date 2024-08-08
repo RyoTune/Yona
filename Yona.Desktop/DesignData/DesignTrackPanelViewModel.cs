776488b885fee7378c810282bbc02fc2a0280cf4
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-07 21:21:05 -0500

    Song name in track panel uses constant height.

--- Yona.Desktop/DesignData/DesignTrackPanelViewModel.cs
+++ Yona.Desktop/DesignData/DesignTrackPanelViewModel.cs
@@ -1,11 +1,12 @@
-﻿using Yona.Core.ViewModels.TrackPanel;
+﻿using System;
+using Yona.Core.ViewModels.TrackPanel;
 
 namespace Yona.Desktop.DesignData;
 
 internal class DesignTrackPanelViewModel : TrackPanelViewModel
 {
     public DesignTrackPanelViewModel()
-        : base(new() { Name = "Test Track", Category = "Category", Tags = ["Tag 1", "Tag 2", "Tag 3"], OutputPath = "Test/Output/File.hca" }, null!)
+        : base(new() { Name = Guid.NewGuid().ToString(), Category = "Category", Tags = ["Tag 1", "Tag 2", "Tag 3"], OutputPath = "Test/Output/File.hca" }, null!)
     {
     }
 }
