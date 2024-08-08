2b2bbe1f57b46128b351668a408985db5f23946a
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-07 22:55:20 -0500

    Update repositories.

---
+++ Yona.Core/Projects/ProjectRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.ObjectModel;
+using Yona.Core.App;
+using Yona.Core.Projects.Models;
+
+namespace Yona.Core.Projects;
+
+public class ProjectRepository
+{
+    private readonly ILogger log;
+    private readonly string projectsDir;
+    private readonly ObservableCollection<ProjectBundle> _projects = [];
+
+    public ProjectRepository(AppService app, ILogger log)
+    {
+        this.log = log;
+        this.projectsDir = Path.Join(app.BaseDir, "projects");
+        Directory.CreateDirectory(this.projectsDir);
+
+        this.LoadProjects();
+    }
+
+    public IReadOnlyList<ProjectBundle> Items => this._projects;
+
+    public void Delete(ProjectBundle entity)
+    {
+        var projectDir = Path.GetDirectoryName(entity.File)!;
+        if (Directory.Exists(projectDir))
+        {
+            Directory.Delete(projectDir, true);
+        }
+
+        this._projects.Remove(entity);
+    }
+
+    public void Create(ProjectData project)
+    {
+        var newProjectFile = Path.Join(this.projectsDir, project.Id, "project.yaml");
+        Directory.CreateDirectory(Path.GetDirectoryName(newProjectFile)!);
+
+        var newProject = new ProjectBundle(newProjectFile)
+        {
+            Data = project,
+        };
+
+        newProject.Save();
+        this._projects.Add(newProject);
+    }
+
+    public ProjectBundle Get(string id) => this._projects.First(x => x.Data.Id == id);
+
+    private void LoadProjects()
+    {
+        foreach (var dir in Directory.EnumerateDirectories(this.projectsDir))
+        {
+            var projectFile = Path.Join(dir, "project.yaml");
+            if (File.Exists(projectFile))
+            {
+                try
+                {
+                    var project = new ProjectBundle(projectFile);
+                    this._projects.Add(project);
+                }
+                catch (Exception ex)
+                {
+                    this.log.LogError(ex, "Failed to load project.\nFile: {file}", projectFile);
+                }
+            }
+        }
+    }
+}
---
+++ Yona.Core/Projects/TemplateRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.ObjectModel;
+using Yona.Core.App;
+using Yona.Core.Projects.Models;
+
+namespace Yona.Core.Projects;
+
+public class TemplateRepository
+{
+    private readonly ILogger? log;
+    private readonly string templatesDir;
+    private readonly ObservableCollection<ProjectBundle> _templates = [];
+
+    public TemplateRepository(AppService app, ILogger log)
+    {
+        this.log = log;
+
+        this.templatesDir = Path.Join(app.BaseDir, "templates");
+        Directory.CreateDirectory(this.templatesDir);
+
+        LoadTemplates();
+    }
+
+    public IReadOnlyList<ProjectBundle> Items => this._templates;
+
+    private void LoadTemplates()
+    {
+        foreach (var dir in Directory.EnumerateDirectories(this.templatesDir))
+        {
+            var templateFile = Path.Join(dir, "project.yaml");
+            if (File.Exists(templateFile))
+            {
+                try
+                {
+                    var template = new ProjectBundle(templateFile);
+                    this._templates.Add(template);
+                }
+                catch (Exception ex)
+                {
+                    this.log?.LogError(ex, "Failed to load project template file.\nFile: {file}", templateFile);
+                }
+            }
+        }
+    }
+}
