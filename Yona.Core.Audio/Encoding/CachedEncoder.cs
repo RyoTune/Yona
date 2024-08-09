c9d6ec89c5423cc7f8d9e0afa91a8ab866d99257
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-08 23:25:24 -0500

    Add cached encoder.

---
+++ Yona.Core.Audio/Encoding/CachedEncoder.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Logging;
+using Yona.Core.Audio.Loops;
+using Yona.Core.Utils;
+
+namespace Yona.Core.Audio.Encoding;
+
+/// <summary>
+/// Encoder wrapper that caches encoded files.
+/// </summary>
+public class CachedEncoder : IEncoder
+{
+    private readonly ILogger? log;
+    private readonly IEncoder encoder;
+    private readonly string cachedFolder;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CachedEncoder"/> class.
+    /// </summary>
+    /// <param name="encoder"></param>
+    /// <param name="cachedFolder"></param>
+    /// <param name="log"></param>
+    public CachedEncoder(IEncoder encoder, string cachedFolder, ILogger? log = null)
+    {
+        this.log = log;
+        this.encoder = encoder;
+        this.cachedFolder = cachedFolder;
+    }
+
+    public string EncodedExt => this.encoder.EncodedExt;
+
+    public string[] InputTypes => this.encoder.InputTypes;
+
+    public async Task Encode(string inputFile, string outputFile, Loop loop)
+    {
+        var inputChecksum = Checksum.Compute(inputFile);
+
+        var cachedFile = Path.Join(this.cachedFolder, $"{inputChecksum}{this.encoder.EncodedExt}");
+        var cachedLoop = loop ?? new();
+        var cachedLoopFile = Path.ChangeExtension(cachedFile, ".dat");
+
+        if (!File.Exists(cachedFile) || LoopChanged(cachedLoopFile, cachedLoop))
+        {
+            this.log?.LogDebug("Cached file missing or loop mismatch.");
+            await this.encoder.Encode(inputFile, cachedFile, cachedLoop);
+            LoopSerializer.SerializeFile(cachedLoopFile, cachedLoop);
+        }
+
+        // Copy cached file to output.
+        await Task.Run(() =>
+        {
+            File.Copy(cachedFile, outputFile, true);
+        });
+    }
+
+    private static bool LoopChanged(string loopFile, Loop loop)
+    {
+        if (!File.Exists(loopFile))
+        {
+            return true;
+        }
+
+        var cachedLoop = LoopSerializer.DeserializeFile(loopFile);
+
+        return cachedLoop.StartSample != loop.StartSample
+            || cachedLoop.EndSample != loop.EndSample
+            || cachedLoop.Enabled != loop.Enabled;
+    }
+}
---
+++ Yona.Core.Audio/Encoding/IEncoder.cs
@@ -0,0 +1,26 @@
+﻿using Yona.Core.Audio.Loops;
+
+namespace Yona.Core.Audio.Encoding;
+
+public interface IEncoder
+{
+    /// <summary>
+    /// Gets default file extension of encoded file.
+    /// </summary>
+    string EncodedExt { get; }
+
+    /// <summary>
+    /// Gets list of valid input formats by extension.
+    /// </summary>
+    string[] InputTypes { get; }
+
+    /// <summary>
+    /// Encode file with loop. If loop is enabled but both loop points are zero,
+    /// the entire file will loop.
+    /// </summary>
+    /// <param name="inputFile">File to encode.</param>
+    /// <param name="outputFile">Output encoded file.</param>
+    /// <param name="loop">Loop settings.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task Encode(string inputFile, string outputFile, Loop loop);
+}
