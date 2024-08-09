84db1acbbcc2da4d2073da742c619c10d6115760
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-08 20:08:54 -0500

    Update track panel view.

--- Yona.Desktop/Views/TrackPanel/TrackPanelView.axaml.cs
+++ Yona.Desktop/Views/TrackPanel/TrackPanelView.axaml.cs
@@ -1,11 +1,20 @@
-using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System.Reactive.Disposables;
+using Yona.Core.ViewModels.TrackPanel;
+using Yona.Desktop.Extensions;
 
 namespace Yona.Desktop.Views.TrackPanel;
 
-public partial class TrackPanelView : UserControl
+public partial class TrackPanelView : ReactiveUserControl<TrackPanelViewModel>
 {
     public TrackPanelView()
     {
         InitializeComponent();
+
+        this.WhenActivated(disposables =>
+        {
+            this.ViewModel!.ShowSelectFile.RegisterHandler(this.HandleFileSelect).DisposeWith(disposables);
+        });
     }
 }
