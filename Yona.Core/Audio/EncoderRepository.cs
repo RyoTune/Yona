19225253fede3bfbfe9d4432a85bb69df4b4eb5f
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-09 01:26:13 -0500

    Implement encoder repository.

---
+++ Yona.Core/Audio/EncoderRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.ObjectModel;
+using Yona.Core.App;
+using Yona.Core.Audio.Encoding;
+using Yona.Core.Audio.Encoding.VGAudio;
+
+namespace Yona.Core.Audio;
+
+public class EncoderRepository
+{
+    private readonly string encodersDir;
+    private readonly ILogger log;
+
+    public EncoderRepository(AppService app, ILogger log)
+    {
+        this.log = log;
+
+        this.encodersDir = Path.Join(app.BaseDir, "audio", "encoders");
+        Directory.CreateDirectory(encodersDir);
+
+        this.LoadEncoders();
+    }
+
+    public ObservableCollection<IEncoder> Items { get; } = [];
+
+    private void LoadEncoders()
+    {
+        this.LoadVgaudioEncoders();
+    }
+
+    private void LoadVgaudioEncoders()
+    {
+        var vgAudioDir = Path.Join(this.encodersDir, "vgaudio");
+        if (!Directory.Exists(vgAudioDir))
+        {
+            return;
+        }
+
+        foreach (var configFile in Directory.EnumerateFiles(vgAudioDir, "*.ini", SearchOption.AllDirectories))
+        {
+            try
+            {
+                var encoder = new VgAudioEncoder(configFile);
+                this.Items.Add(encoder);
+            }
+            catch (Exception ex)
+            {
+                this.log.LogError(ex, "Failed to load VGAudio INI.\nFile: {configFile}", configFile);
+            }
+        }
+    }
+}
