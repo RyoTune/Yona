95b5b8125f10fcfcfb8373f84c6dbc75dccdeb99
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-08 20:00:40 -0500

    Fix RxUI notifications not working.

--- Yona.Core/ViewModels/ViewModelBase.cs
+++ Yona.Core/ViewModels/ViewModelBase.cs
@@ -6,6 +6,12 @@ namespace Yona.Core.ViewModels;
 
 public partial class ViewModelBase : ObservableObject, IReactiveObject
 {
+    public ViewModelBase()
+    {
+        this.SubscribePropertyChangingEvents();
+        this.SubscribePropertyChangedEvents();
+    }
+
     public void RaisePropertyChanged(PropertyChangedEventArgs args) => this.OnPropertyChanged(args);
 
     public void RaisePropertyChanging(PropertyChangingEventArgs args) => this.OnPropertyChanging(args);
