4434a1d9e4a6b884c8ed1b879f01074987c4616d
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-08 23:25:55 -0500

    Add tests library.

---
+++ Yona.Tests/Core.Audio/LoopSerializerTests.cs
@@ -0,0 +1,20 @@
+﻿using Yona.Core.Audio.Loops;
+
+namespace Yona.Tests.Core.Audio;
+
+public class LoopSerializerTests
+{
+    [Fact]
+    public void LoopSerializer_Serialization()
+    {
+        var ogLoop = new Loop() { StartSample = 42, EndSample = 100, Enabled = true };
+        var memStream = new MemoryStream();
+
+        LoopSerializer.Serialize(memStream, ogLoop);
+        var newLoop = LoopSerializer.Deserialize(new MemoryStream(memStream.ToArray()));
+
+        Assert.Equal(ogLoop.Enabled, newLoop.Enabled);
+        Assert.Equal(ogLoop.StartSample, newLoop.StartSample);
+        Assert.Equal(ogLoop.EndSample, newLoop.EndSample);
+    }
+}
