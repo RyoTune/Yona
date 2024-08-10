dc5bbdc32629d82bdd9419e042159208ef6e3cd6
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-10 03:08:03 -0500

    Add interaction types for common dialogs.

---
+++ Yona.Core/Common/Dialog/DialogInteractions.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace Yona.Core.Common.Dialog;
+
+public class FileSelectInteraction : Interaction<FileSelectOptions, string[]>
+{
+}
+
+public class FolderSelectInteraction : Interaction<FolderSelectOptions, string[]>
+{
+}
+
+public class SaveFileInteraction : Interaction<SaveFileOptions, string[]>
+{
+}
