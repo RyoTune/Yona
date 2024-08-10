4bfa4f8dec086400ba7b1a62f9e0415c2edba946
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-10 03:07:29 -0500

    Add SavableFile extension for setting up auto-save.

---
+++ Yona.Core/Extensions/SavableFileExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DynamicData.Binding;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using Yona.Core.Common;
+
+namespace Yona.Core.Extensions;
+
+public static class SavableFileExtensions
+{
+    public const int SAVE_BUFFER_MS = 250;
+
+    public static IDisposable AutosaveWithChanges<T>(this SavableFile<T> savable)
+        where T : INotifyPropertyChanged, new() =>
+        savable.Data.WhenAnyPropertyChanged()
+            .Throttle(TimeSpan.FromMilliseconds(SAVE_BUFFER_MS))
+            .Subscribe(_ => savable.Save());
+}
