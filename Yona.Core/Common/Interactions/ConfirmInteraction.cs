6bbed0729cb0ca147dc6edf5cd5c0ebb1e76ed13
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-11 02:18:47 -0500

    Add confirm interaction types.

---
+++ Yona.Core/Common/Interactions/ConfirmInteraction.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Yona.Core.Common.Interactions;
+
+public class ConfirmInteraction : Interaction<ConfirmOptions, bool>
+{
+}
---
+++ Yona.Core/Common/Interactions/ConfirmOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Yona.Core.Common.Interactions;
+
+public class ConfirmOptions
+{
+    public required string Title { get; set; }
+
+    public string? Description { get; set; }
+}
