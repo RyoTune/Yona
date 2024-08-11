89cecd982721c0c8c05848c03fade02095623787
MirrorTuneZM <zutomayofan50@gmail.com>
2024-08-11 02:31:26 -0500

    Setup dashboard switching.

--- Yona.Desktop/Views/Dashboard/DashboardView.axaml.cs
+++ Yona.Desktop/Views/Dashboard/DashboardView.axaml.cs
@@ -1,15 +1,33 @@
-using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
 using SukiUI.Controls;
+using System;
+using System.Reactive.Disposables;
+using Yona.Core.Settings.Models;
 using Yona.Core.ViewModels.Dashboard;
+using Yona.Core.ViewModels.Dashboard.Projects;
 
 namespace Yona.Desktop.Views.Dashboard;
 
-public partial class DashboardView : UserControl
+public partial class DashboardView : ReactiveUserControl<DashboardViewModel>
 {
     public DashboardView()
     {
         InitializeComponent();
+
+        this.WhenActivated((CompositeDisposable disposables) =>
+        {
+            this.ViewModel!.Projects.Router.CurrentViewModel
+            .Subscribe(x =>
+            {
+                if (x is ProjectTracksViewModel && this.sukiSideMenu.SelectedItem is not ProjectsViewModel)
+                {
+                    this.sukiSideMenu.SelectedIndex = (int)Page.Projects;
+                }
+            })
+            .DisposeWith(disposables);
+        });
     }
 
     protected override void OnLoaded(RoutedEventArgs e)
